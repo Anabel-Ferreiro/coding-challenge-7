321faf7d580a8ddeff045a88d324c0d349a6fba5
Anabel Ferreiro Perez <aferreiroperez@usf.edu>
2026-06-13 16:59:12 -0400

    fix spacing in DOM manipulation

--- cc_7.js
+++ cc_7.js
@@ -1,8 +1,8 @@
 // Step 5: DOM manipulation
 // Select elements 
-const ctaInput = document.getElementById ("cta-input");
-const ctaButton = document.getElementById ("cta-button");
-const ctaHeadline = document.getElementById ("cta");
+const ctaInput = document.getElementById("cta-input");
+const ctaButton = document.getElementById("cta-button");
+const ctaHeadline = document.getElementById("cta");
 
 // Add event listener to update CTA text
 ctaButton.addEventListener("click", function() {
